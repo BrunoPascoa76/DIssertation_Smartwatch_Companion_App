90927c9466520c45dc429f40afaf0d0b284c5847
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-11-17 17:18:52 +0000

    I made Kafka Helper use the same interface as the original

--- app/src/main/java/cm/project/dissertationsmartwatchcompanionapp/presentation/KafkaHelper.kt
+++ app/src/main/java/cm/project/dissertationsmartwatchcompanionapp/presentation/KafkaHelper.kt
@@ -1,14 +1,23 @@
 package cm.project.dissertationsmartwatchcompanionapp.presentation
 
-import android.util.Log
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
-import java.util.Properties
+import okhttp3.Response
+import java.io.IOException
 
-object KafkaRESTHelper {
+object KafkaHelper {
+    private var client:OkHttpClient?=null
+    private var brokerURL: String=""
+
+    fun initProducer(brokerURL: String){
+        this.brokerURL=brokerURL
+        this.client=OkHttpClient()
+    }
+
     fun sendReading(brokerURL:String,readingType: String, value: Int){
         val timestamp=System.currentTimeMillis() *1000 //since we can't get the same measure in nanoseconds (only elapsed)
         val json="""{
@@ -17,16 +26,32 @@ object KafkaRESTHelper {
             | "value": $value
             |}""".trimMargin()
 
-        val request_body=json.toRequestBody("application/vnd.kafka.json.v2+json".toMediaTypeOrNull())
+        val requestBody=json.toRequestBody("application/vnd.kafka.json.v2+json".toMediaTypeOrNull())
 
         val request= Request.Builder()
-            .url(brokerURL)
-            .post(request_body)
+            .url("$brokerURL/topics/smartwatch_data")
+            .post(requestBody)
             .build()
+
+        client?.newCall(request)?.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Request failed: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) {
+                        println("Response failed: ${response.code}")
+                    } else {
+                        println("Response success: ${response.body?.string()}")
+                    }
+                }
+            }
+        })
 
     }
 
     fun close(){
-
+        client?.clo
     }
 }
